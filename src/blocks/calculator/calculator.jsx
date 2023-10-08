f913cd9638f036b5994112795f432f43157cae26
Josiah Leon <sjosiahleon@gmail.com>
2023-10-08 15:57:06 -0700

    first interation of calc, should be working now.

--- src/blocks/calculator/calculator.jsx
+++ src/blocks/calculator/calculator.jsx
@@ -17,29 +17,51 @@ const Calculator = () => {
         setRes('0');
         setVal1('0');
         setVal2('0');
-        setWhichVal(true);
+        setWhichVal('');
     };
 
     /**
-     * The function `add_num` takes a number as input and updates a value in state by adding the input
+     * The function `addNum` takes a number as input and updates a value in state by adding the input
      * number to it.
      */
-    const add_num = (num) => {
-        let setVal = whichVal ? setVal1 : setVal2;
+    const addNum = (num) => {
+        let setVal = whichVal === '' ? setVal1 : setVal2;
         setVal(val => {
-            val = val+num
-            val = Number(val)
-            val = String(val)
+            val = val+num;
+            val = String(Number(val));
 
-            setRes(val)
-            return val
-        })
+            setRes(val);
+            return val;
+        });
     };
 
+    /**
+     * The flipPolarity function flips the sign of a value and updates the result.
+     */
+    const flipPolarity = () => {
+        let setVal = whichVal === '' ? setVal1 : setVal2;
+        setVal(val => {
+            val = String(-1 * Number(val))
+            setRes(val);
+            return val;
+        });
+    };
+
+    /* The `solve` function is responsible for evaluating the mathematical expression based on the
+    values of `val1`, `whichVal`, and `val2`. It uses the `eval` function to perform the
+    evaluation and updates the result (`res`) using the `setRes` function. */
+    const solve = () => {
+        if (whichVal === '') { return null }
+        setRes(eval(val1+whichVal+val2))
+    };
+
+    /**
+     * The handleClick function handles different button clicks and performs corresponding actions
+     * based on the clicked button value.
+     * @returns nothing (undefined) in most cases. However, if the targetValue is 'clear', the function
+     * calls the clear() function and then returns.
+     */
     const handleClick = (e) => {
-        // console.log(e.target.value);
-        // perfect... I can make a func to handle all clicks with this.
-        // Thank you propagation
         let targetValue = e.target.value;
 
         if (targetValue === 'clear') {
@@ -50,9 +72,13 @@ const Calculator = () => {
         }
 
         if ('1234567890'.includes(targetValue)) {
-            add_num(targetValue)
+            addNum(targetValue);
+        } else if ('±' === targetValue) {
+            flipPolarity();
+        } else if ('-+*/'.includes(targetValue)) {
+            setWhichVal(targetValue);
         } else {
-            console.log(targetValue);
+            solve()
         }
 
     }
@@ -63,27 +89,26 @@ const Calculator = () => {
                 <div className="calc-res">{res}</div>
 
                 <button className="calc-row0" value='clear'>C</button>
-                <button className="calc-div" value="/">/</button>
+                <button className={`calc-div ${whichVal === '/' ? 'active' : ''}`} value="/">/</button>
 
                 <button className="calc-row1" value="7">7</button>
                 <button className="calc-row1" value="8">8</button>
                 <button className="calc-row1" value="9">9</button>
-                <button className="calc-row1" value="*">*</button>
+                <button className={`calc-row1 ${whichVal === '*' ? 'active' : ''}`} value="*">*</button>
 
                 <button className="calc-row2" value="4">4</button>
                 <button className="calc-row2" value="5">5</button>
                 <button className="calc-row2" value="6">6</button>
-                <button className="calc-row2" value="-">-</button>
+                <button className={`calc-row2 ${whichVal === '-' ? 'active' : ''}`} value="-">-</button>
 
                 <button className="calc-row3" value="1">1</button>
                 <button className="calc-row3" value="2">2</button>
                 <button className="calc-row3" value="3">3</button>
-                <button className="calc-row3" value='+'>+</button>
+                <button className={`calc-row3 ${whichVal === '+' ? 'active' : ''}`} value='+'>+</button>
 
                 <button className="calc-row4" value="0">0</button>
-                <button className="calc-row4" value=".">.</button>
                 <button className="calc-row4" value="±">±</button>
-                <button className="calc-row4" value="=">=</button>
+                <button className="calc-row4" style={{gridColumn: 4}} value="=">=</button>
             </div>
         </div>
     )
