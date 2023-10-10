747f1dfb439055aaed97bdf4ce61e6fc5da2f6c9
Josiah Leon <sjosiahleon@gmail.com>
2023-10-10 09:36:44 -0700

    added two more testcases for the calculator

--- src/blocks/calculator/calculator.test.js
+++ src/blocks/calculator/calculator.test.js
@@ -1,5 +1,5 @@
 import Calculator from "./calculator";
-import {render, screen} from '@testing-library/react';
+import {render, screen, fireEvent} from '@testing-library/react';
 
 it('Res div to start with two 0s', () => {
     render(<Calculator/>);
@@ -7,4 +7,23 @@ it('Res div to start with two 0s', () => {
     expect(screen.findAllByText('0').length === 2);
 });
 
-// it('')
+
+it('Calculator has all its buttons', () => {
+    const buttons = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*', '/', '=', 'C', '±'];
+    render(<Calculator/>);
+
+    expect(screen.findAllByText('0').length === 2);
+    buttons.forEach(button => expect(screen.getByText(button)));
+});
+
+
+it('Flips between positive and negative', () => {
+    render(<Calculator/>);
+    const flipButton = screen.getByText('±');
+    const numButton = screen.getByText('1');
+    fireEvent.click(numButton);
+    fireEvent.click(flipButton);
+    expect(screen.getByText('-1'));
+    fireEvent.click(flipButton);
+    expect(screen.findByText('1'));
+});
