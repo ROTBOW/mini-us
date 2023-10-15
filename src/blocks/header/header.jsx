ab42c34a23dc63827f505a51613a1ba8981622ce
Josiah Leon <sjosiahleon@gmail.com>
2023-10-15 11:43:18 -0700

    added more text to the header

--- src/blocks/header/header.jsx
+++ src/blocks/header/header.jsx
@@ -6,6 +6,7 @@ const Header = () => {
 
     return (
         <div className="header-main">
+            <div><b>Mini-us</b> | A Collection Of Blocks</div>
             <div>
                 Created by <a href="https://github.com/ROTBOW">Josiah Leon</a>
             </div>
