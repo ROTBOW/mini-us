669ec8ce6135e2ec6b4d441ca2d35617b9cca143
Josiah Leon <sjosiahleon@gmail.com>
2023-10-15 12:04:38 -0700

    Added cycling emojis to the header

--- src/blocks/header/header.jsx
+++ src/blocks/header/header.jsx
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './header.scss';
 
 
 const Header = () => {
+    const emojis = [
+        '😲',
+        '😵‍💫',
+        '🥲',
+        '🍵',
+        '😆',
+        '🎮'
+
+    ]
+
+    const [curEmoji, setCurEmoji] = useState(emojis[Math.floor(Math.random() * emojis.length)])
+
+    const randomEmoji = () => {
+        let emoji = emojis[Math.floor(Math.random() * emojis.length)];
+        while (emoji === curEmoji) {
+            emoji = emojis[Math.floor(Math.random() * emojis.length)];
+        };
+
+        return emoji;
+    };
+
+    useEffect(() => {
+        const emojiInterval = setInterval(() => {
+            setCurEmoji(randomEmoji());
+        }, 5000);
+
+        return () => clearInterval(emojiInterval);
+    }, []);
 
     return (
         <div className="header-main">
             <div><b>Mini-us</b> | A Collection Of Blocks</div>
             <div>
-                Created by <a href="https://github.com/ROTBOW">Josiah Leon</a>
+                Created with {curEmoji} by <a href="https://github.com/ROTBOW">Josiah Leon</a>
             </div>
             <div className="header-line"/>
         </div>
