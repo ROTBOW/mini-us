019cc9c34b1633a46e804a75315c412abfad3c5d
Josiah Leon <sjosiahleon@gmail.com>
2023-10-18 00:19:04 -0700

    Added a big red button

--- src/blocks/redButton/redButton.jsx
+++ src/blocks/redButton/redButton.jsx
@@ -1,21 +1,51 @@
-import React from "react";
+import React, {useState, useRef} from "react";
 import './redButton.scss';
 
 
 const RedButton = () => {
 
+    const [countDown, setCountDown] = useState(10);
+    const [counting, setCounting] = useState(false);
+
+    const countRef = useRef(countDown);
+    countRef.current = countDown;
+
+    /**
+     * The handleClick function is a timer that counts down from a specified number and updates the
+     * countdown display until it reaches 1, at which point it displays a message.
+     */
     const handleClick = (e) => {
         e.preventDefault();
+        if (counting !== false) { return }
+        setCounting(true);
         e.target.classList.add('redbutton-actived')
-        // console.log(e.target.classList);
+
+        const tick = () => {
+            setTimeout(() => {
+                
+                setCountDown(count => count-1)
+
+                if (countRef.current > 1) { 
+                    tick();
+                } else {
+                    e.target.classList.add('redbutton-poof')
+                    setCountDown('Silly Goose');
+                }
+            }, 1000);
+        }
+        tick();
     };
+
 
     return (
         <div className="redbutton-block">
+            
             <div className="redbutton-base">
-                <button className="redbutton-button" onClick={handleClick}></button>
+                <div/>
+                <button onClick={handleClick}></button>
             </div>
-            <div className="redbutton-counter">numbers here</div>
+            
+            <div className="redbutton-counter">{countDown}</div>
         </div>
     )
 
