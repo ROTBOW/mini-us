d55b9344b307b6ed848e68ae23bf3482e6f1c782
Josiah Leon <sjosiahleon@gmail.com>
2023-10-19 12:39:58 -0700

    first iteration of the notes block

--- src/blocks/notes/notes.jsx
+++ src/blocks/notes/notes.jsx
@@ -1,32 +1,116 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import './notes.scss';
 
+const getNoteCount = () => {
+    let count = 0;
 
+    for (let i in localStorage) {
+        if (i.startsWith('notes')) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+const grabNotes = () => {
+    let notes = {};
+    for (let i in localStorage) {
+        if (i.startsWith('notes')) {
+            notes[i] = localStorage.getItem(i)
+        }
+    }
+
+    return notes;
+};
 
 const Notes = () => {
     const [mode, setMode] = useState('writer');
+    const [currNote, setCurrNote] = useState('notes-0');
+    const [writerText, setWriterText] = useState('');
+    const [notes, setNotes] = useState({});
 
     const handleModeChange = (e) => {
-        e.stopPropagation()
-        setMode(e.target.value)
+        e.stopPropagation();
+        if (e.target.value === 'reader' && Object.keys(notes).length === 0) { return };
+        setMode(e.target.value);
     };
 
+    const handleNewNote = (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        
+        let count = getNoteCount()
+        localStorage.setItem(`notes-${count}`, writerText);
+        setMode(() => {
+            setCurrNote(`notes-${count}`);
+            setNotes(grabNotes());
+            setWriterText('');
+            return 'reader'
+        })
+
+    };
+
+    useEffect(() => {
+        setNotes(grabNotes());
+    }, []);
+
     const writer = (
-        <>
-        <textarea></textarea>
-        <button>create note</button>
-        </>
+        <div className="notes-writer">
+            <textarea defaultValue={writerText} onChange={e => setWriterText(e.target.value)} />
+            <button onClick={handleNewNote}>Create Note</button>
+        </div>
     );
 
+    const createReaderIdx = () => {
+        let idxs = [];
+        let idx = 1;
+        
+        for (let i in notes) {
+            idxs.push(
+                <li
+                    key={i}
+                    onClick={()=>(setCurrNote(i))}
+                    className={currNote === i ? 'notes-active' : ''}
+                    >
+                    {idx}
+                </li>
+            );
+            idx++;
+        };
+
+        return idxs;
+    };
+
     const reader = (
-        <div>this is the reader</div>
+        <div className="notes-reader">
+            <ol>
+                {createReaderIdx()}
+            </ol>
+            <div>
+                {notes[currNote]}
+            </div>
+        </div>
     );
 
     return (
         <div className="notes-block">
             <div className="notes-header-buttons">
-                <button onClick={handleModeChange} value="writer" className={mode === 'writer' ? 'notes-active' : ''}>writer</button>
-                <button onClick={handleModeChange} value="reader" className={mode === 'reader' ? 'notes-active' : ''}>reader</button>
+                <button
+                    onClick={handleModeChange}
+                    value="writer"
+                    className={mode === 'writer' ? 'notes-active' : ''}
+                >writer</button>
+                <button
+                    onClick={handleModeChange}
+                    value="reader"
+                    className={
+                        `
+                            ${mode === 'reader' ? 'notes-active' : ''}
+                            ${Object.keys(notes).length === 0 ? 'notes-deactivate' : ''}
+                        `
+                    }
+                >reader</button>
             </div>
             {mode === 'writer' ? writer : reader}
         </div>
